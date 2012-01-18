2cc4924e1062b76ad2eeb1a858b78a54cd4318ac
ianwookim <ianwookim@gmail.com>
2012-01-18 08:41:46 +0000

    start webcanvas

---
+++ canvas.js
@@ -0,0 +1,197 @@
+dojo.require("dijit.MenuBar"); 
+dojo.require("dijit.PopupMenuBarItem"); 
+dojo.require("dijit.Menu"); 
+dojo.require("dijit.MenuItem"); 
+dojo.require("dijit.PopupMenuItem"); 
+dojo.require("dijit.MenuSeparator"); 
+dojo.require("dijit.CheckedMenuItem"); 
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.layout.BorderContainer");
+
+var canvasScaleX = 1;
+var canvasScaleY = 1; 
+
+
+var mousedownenter = false ; 
+//var currentstroke ; 
+
+
+var tools_mode = "pen"; // 0 SELECT 
+               // 1 PEN 
+               // 2 ERASE
+
+//window.onload = 
+window.onresize = function () { 
+    //    var C = 0.8 ; 
+    //    var W_TO_H = 2/1; 
+    var el = document.getElementById('tutorial'); 
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+
+    var canvasWidth = el.offsetWidth;
+    var canvasHeight = el.offsetHeight ;
+    console.log("in onresize"); 
+    console.log(viewportWidth, viewportHeight); 
+
+    //    el.style.position = "fixed";
+    el.setAttribute("width", canvasWidth);
+    el.setAttribute("height", canvasHeight);
+    //    el.style.top = 0 ; //(viewportHeight - canvasHeight) / 2;
+    //    el.style.left = 0 ;//(viewportWidth - canvasWidth) / 2;
+
+} 
+
+
+function tools_pen() { 
+    tools_mode = "pen"; 
+    console.log("pen is selected"); 
+}
+
+function tools_erase() {
+    tools_mode = "erase" ; 
+    console.log("erase is selected"); 
+}
+
+function tools_highlighter() { 
+    console.log("highligher is not implemented yet."); 
+}
+
+function tools_text() { 
+    console.log("text is not implemented yet."); 
+}
+
+getPosition = function (obj) {
+    var x = 0, y = 0;
+    if (obj.offsetParent) {
+        do {
+            x += obj.offsetLeft;
+            y += obj.offsetTop;
+            obj = obj.offsetParent;
+        } while (obj);
+    }
+    return {'x': x, 'y': y};
+};
+
+getSize = function (obj) {
+    var x = 0, y = 0;
+    x = obj.offsetWidth ;
+    y = obj.offsetHeight ; 
+    return {'w': x, 'h': y};
+};
+
+// Get mouse event position in DOM element
+getEventPosition = function (e, obj, scale) {
+    var evt, docX, docY, pos;
+    //if (!e) evt = window.event;
+    evt = (e ? e : window.event);
+    if (evt.pageX || evt.pageY) {
+        docX = evt.pageX;
+        docY = evt.pageY;
+    } else if (evt.clientX || evt.clientY) {
+        docX = evt.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+        docY = evt.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+    }
+    pos = getPosition(obj);
+    if (typeof scale === "undefined") {
+        scale = 1;
+    }
+    return {'x': (docX - pos.x) / scale, 'y': (docY - pos.y) / scale};
+};
+
+function init() { 
+    var canvas = document.getElementById('tutorial') ; 
+
+    var canvasWidth = canvas.offsetWidth; 
+    var canvasHeight = canvas.offsetHeight;
+    console.log(canvasWidth, canvasHeight); 
+
+    //    el.style.position = "fixed";
+    canvas.setAttribute("width", canvasWidth);
+    canvas.setAttribute("height", canvasHeight);
+    //    el.style.top = 0 ; //(viewportHeight - canvasHeight) / 2;
+
+    canvas.onselectstart = function() { return false; }
+    canvas.addEventListener('mousedown',ev_mousedown,false); 
+    canvas.addEventListener('mousemove',ev_mousemove,false);
+    canvas.addEventListener('mouseup',ev_mouseup,false); 
+    canvas.addEventListener('mousedoubleclick',ev_mousedclick,false); 
+    
+    draw(); 
+
+
+}
+
+function draw() {
+    console.log('draw event fired'); 
+
+    var canvas = document.getElementById('tutorial'); 
+    if (canvas.getContext) { 
+	var ctx = canvas.getContext('2d'); 
+	
+	ctx.fillStyle="rgb(200,0,0)"; 
+	ctx.fillRect (10,10,55,50);
+	
+	ctx.fillStyle ="rgba(0,0,200,0.5)";
+	ctx.fillRect (30,30,55,50);
+
+	ctx.fillStyle ="rgba(0,200,200,0.5)";
+	ctx.fillRect (50,40,55,50);
+
+    }
+}
+
+function ev_mousedclick(ev) { 
+    console.log('double clicked'); 
+}
+
+function ev_mousedown(ev) {
+    var eventpos = getEventPosition( ev, this ) ; 
+    x = eventpos.x ; 
+    y = eventpos.y ;
+
+    mousedownenter = true ; 
+
+    //    console.log(x,y);
+    //    console.log('mouse button down');
+
+    var canvas = document.getElementById('tutorial'); 
+
+    console.log(getSize(canvas)) ;
+
+    if (canvas.getContext) { 
+	var ctx = canvas.getContext('2d'); 
+        ctx.beginPath(); 
+	ctx.moveTo(x,y) ;  
+    }
+}
+
+function ev_mousemove(ev) {
+    if( mousedownenter ) { 
+	var eventpos = getEventPosition( ev, this ) ; 
+	x = eventpos.x ; 
+	y = eventpos.y ;
+	
+	//	console.log(x,y);
+	var canvas = document.getElementById('tutorial'); 
+	if (canvas.getContext) { 
+	    var ctx = canvas.getContext('2d'); 
+	    ctx.lineTo(x,y) ;  
+	    ctx.stroke(); 	    
+	}
+    }
+}
+
+function ev_mouseup(ev) {
+    var eventpos = getEventPosition( ev, this ) ; 
+    x = eventpos.x ; 
+    y = eventpos.y ;
+
+    mousedownenter = false ;
+
+    //    console.log(x,y);
+
+    //    console.log('mouse button up'); 
+
+} 
